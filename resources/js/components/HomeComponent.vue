c58cbf1a2cfe8def88137fd82a91d7ac8d4a9f31
jefriR <jefrironaldo48@yahoo.com>
2019-10-15 21:14:48 +0700

    First

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,28 @@
+<template>
+<!--    <div class="row justify-content-center">-->
+<!--        <div class="col-md-8">-->
+<!--            <div class="card card-default">-->
+<!--                <div class="card-header">Home Component</div>-->
+
+<!--                <div class="card-body">-->
+<!--                    I'm the Home Component component.-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae eos modi nulla quidem sapiente similique, tempore vel! Amet aut dolore ipsa itaque nisi possimus quos repellendus, suscipit tenetur vel voluptatum?
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
